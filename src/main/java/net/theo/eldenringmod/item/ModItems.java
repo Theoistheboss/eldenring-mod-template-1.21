86d377f66c9c9a94df2b5945c3ade4919c6f59fb
theot <theot1856@magic.school.nz>
2025-08-04 09:33:45 +1200

    Intital commit

--- src/main/java/net/theo/eldenringmod/item/ModItems.java
+++ src/main/java/net/theo/eldenringmod/item/ModItems.java
@@ -7,6 +7,8 @@ import net.minecraft.util.Identifier;
 import net.theo.eldenringmod.EldenringMod;
 
 public class ModItems {
+    public static final Item PEBBLE = registeritem("pebble", new Item( new Item.Settings()));
+
 
     public static Item registeritem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(EldenringMod.MOD_ID, name), item);
