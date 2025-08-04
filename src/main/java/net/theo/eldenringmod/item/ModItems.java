b6bb65b5b8b26fcde0415b2891018bb0e86606bc
theot <theot1856@magic.school.nz>
2025-08-05 10:40:46 +1200

    Intital commit

--- src/main/java/net/theo/eldenringmod/item/ModItems.java
+++ src/main/java/net/theo/eldenringmod/item/ModItems.java
@@ -1,6 +1,9 @@
 package net.theo.eldenringmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,5 +19,10 @@ public class ModItems {
 
     public static void RegisterModItems() {
         EldenringMod.LOGGER.info("Registering Mod Items For " + EldenringMod.MOD_ID);
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PEBBLE);
+        });
     }
 }
